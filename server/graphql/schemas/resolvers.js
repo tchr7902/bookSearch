b0f810a4ce550eed09c80eb14aa8cbea3198d31c
Trevor Christensen <trevorchristensen3405@gmail.com>
2024-02-10 18:58:21 -0700

    working on resolvers and typeDefs

---
+++ server/graphql/schemas/resolvers.js
@@ -0,0 +1,55 @@
+const { User, Book } = require('../../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user.id}).populate('savedBooks');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in!');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne({ email});
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect email or password!');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect email or password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, {bookData}, context) => {
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true }
+                ).populate('savedBooks');
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in to remove books!');
+        },
+    },
+};
+
+module.exports = resolvers;

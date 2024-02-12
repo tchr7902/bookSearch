918daf08cf747e891117084da73962e6cbc6dbdf
Trevor Christensen <trevorchristensen3405@gmail.com>
2024-02-12 15:02:57 -0700

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve'; // Import the nodeResolve plugin
 
 export default defineConfig({
-  plugins: [react(), nodeResolve()],
+  plugins: [
+    react(),
+    nodeResolve(), // Include the nodeResolve plugin
+  ],
   server: {
     port: 3000,
     open: true,

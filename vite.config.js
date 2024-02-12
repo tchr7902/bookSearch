126516fa27fd2eab80be281c86428132b3feb88c
Trevor Christensen <trevorchristensen3405@gmail.com>
2024-02-12 16:16:56 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: 'build', // Specify the output directory for built assets
     rollupOptions: {
       input: {
         // Specify the entry points for both client and server-side code
